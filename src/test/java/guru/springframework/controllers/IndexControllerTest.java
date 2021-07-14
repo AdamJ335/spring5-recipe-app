27891a7675404555e04b96283595cd3fa5d4448d
Adam Jennings <ajennings@tier2consulting.com>
2021-07-14 16:49:31 +0100

    Allows updating Recipe

--- src/test/java/guru/springframework/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -47,7 +47,7 @@ public class IndexControllerTest {
 
 
     @Test
-    public void getIndexPage() {
+    public void getIndexPage() throws Exception {
 
         //given
         Set<Recipe> recipes = new HashSet<>();
@@ -70,6 +70,6 @@ public class IndexControllerTest {
         verify(recipeService, times(1)).getRecipes();
         verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
         Set<Recipe> setInController = argumentCaptor.getValue();
-        assertEquals(2,setInController.size());
+        assertEquals(2, setInController.size());
     }
 }
